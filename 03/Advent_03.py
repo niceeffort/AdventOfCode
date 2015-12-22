47735d5fb8ffbe1072f20ca055b4e79053bf9893
David Schoemehl <davidschoemehl@gmail.com>
2015-12-21 21:31:39 -0600

    Solution for part 2 day three

--- 03/Advent_03.py
+++ 03/Advent_03.py
@@ -5,13 +5,29 @@ text =  f.readline()
 #track x and y positions
 x=0
 y=0
+SantaX = 0
+SantaY = 0
+RoboSantaX = 0
+RoboSantaY = 0
 
 xValues = []
 yValues = []
 
 map = { '0,0' : 1 }
 
+SantaTurn = 1
+
 for char in range(0,len(text)):
+
+    if(SantaTurn):
+        #print 'SantaX, SantaY = ' + str(SantaX) + ',' + str(SantaY)
+        x = SantaX
+        y = SantaY
+    else:
+        #print 'RoboSantaX, RoboSantaY = ' + str(RoboSantaX) + ',' + str(RoboSantaY)
+        x = RoboSantaX
+        y = RoboSantaY
+
     if (text[char] == '^'):
         y += 1
     elif(text[char] == 'v'):
@@ -20,18 +36,21 @@ for char in range(0,len(text)):
         x += 1
     elif(text[char] == '<'):
         x -= 1
-    xValues.append(x)
-    yValues.append(y)
 
     pair = str(x) + ',' + str(y)
     if (pair in map):
-        print 'pair exists'
         map[pair] += 1
     else:
         map[pair] = 1
 
-print xValues
-print yValues
+    if(SantaTurn):
+        SantaX = x
+        SantaY = y
+        SantaTurn = 0
+    else:
+        RoboSantaX = x
+        RoboSantaY = y
+        SantaTurn = 1
 
 #Create a dictionary and count pairs
-print len(map)
+print 'Number of houses delivered = ' + str(len(map))
