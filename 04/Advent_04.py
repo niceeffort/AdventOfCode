1289804de311a4cd8097b8a8021bdc4c1d09bb1c
David Schoemehl <davidschoemehl@gmail.com>
2015-12-21 21:59:17 -0600

    Solution for part 1 day four

---
+++ 04/Advent_04.py
@@ -0,0 +1,25 @@
+import hashlib
+
+f = open('input_04.txt')
+text =  f.readline().rstrip()
+
+m = hashlib.md5()
+m.update('abcdef609043')
+print m.hexdigest()
+
+answerNotFound = 1
+counter = 0
+#text = 'abcdef'
+while( answerNotFound ):
+    counter += 1
+    m = hashlib.md5()
+    tempText = text
+    tempText += str(counter)
+    print 'tempText = ' + tempText
+    m.update( tempText )
+    print m.hexdigest()[:5]
+    if( str(m.hexdigest())[:5] == '00000'):
+        answerNotFound = 0
+        print 'Final Answer Hex = ' + str(m.hexdigest())
+        print 'Final Answer = ' + str(counter)
+
