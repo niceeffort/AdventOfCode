a373ae00330023b7017ab1f87f2f0810a5aeee4d
David Schoemehl <davidschoemehl@gmail.com>
2016-12-03 10:14:40 -0600

    Part 2, find existing position.

--- 2016/01/AOC_2016_01.py
+++ 2016/01/AOC_2016_01.py
@@ -20,8 +20,10 @@ direction_map = {'NORTH':[1,1],
 
 #distance traveled [x,y]
 distance_traveled = [0,0]
+location_map = []
 
 direction = 'NORTH'
+duplicate_found = 0
 for next_move in movement:
     if direction == 'NORTH':
         if next_move[0] == 0:
@@ -47,8 +49,21 @@ for next_move in movement:
     distance_traveled[direction_map[direction][0]] += next_move[1] * direction_map[direction][1]
     #print distance_traveled
 
+    #look for existing position
+    if distance_traveled in location_map:
+        print 'location_found'
+        print distance_traveled
+        duplicate_found = 1
 
-#print distance_traveled
+    if duplicate_found:
+        break
+
+    location_map.append([distance_traveled[0],distance_traveled[1]])
+    print 'loc_map' + str(location_map)
+
+
+print distance_traveled
 total_distance = abs(distance_traveled[0]) + abs(distance_traveled[1])
 print 'total_distance = ' + str(total_distance)
+print location_map
 
