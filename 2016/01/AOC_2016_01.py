c36eb715dc7bf434ea654d10aaa26862d5187388
David Schoemehl <davidschoemehl@gmail.com>
2016-12-03 22:10:02 -0600

    Part 2, finally got the right answer

--- 2016/01/AOC_2016_01.py
+++ 2016/01/AOC_2016_01.py
@@ -25,6 +25,8 @@ location_map = []
 direction = 'NORTH'
 duplicate_found = 0
 first_duplicate = []
+positions = []
+count = 0
 for next_move in movement:
     if direction == 'NORTH':
         if next_move[0] == 0:
@@ -47,20 +49,44 @@ for next_move in movement:
         else:
             direction = 'NORTH'
 
-    distance_traveled[direction_map[direction][0]] += next_move[1] * direction_map[direction][1]
+
+    step = 1
+
+    while step <= next_move[1]:
+        distance_traveled[direction_map[direction][0]] += 1 * direction_map[direction][1]
+
+        #print 'positions = ' + str(positions)
+
+        print 'count = ' + str(count)
+        if duplicate_found == 0:
+            print 'dist_traveled =' + str(distance_traveled)
+            print 'positions = ' + str(positions)
+            if distance_traveled in positions:
+                print 'Dupilcate Found!'
+                first_duplicate = [distance_traveled[0],distance_traveled[1]]
+                print 'first_duplicate' + str(first_duplicate)
+                duplicate_found = 1
+            else:
+                positions.append([distance_traveled[0], distance_traveled[1]])
+
+        step +=1
+        count +=1
     #print distance_traveled
 
+    #store a grid of every location you have visited. Once you find the match calculate the distance from 0
+
+
     #look for existing position
-    if duplicate_found == 0:
-        if distance_traveled in location_map:
-            print 'location_found'
-            print distance_traveled
-            first_duplicate = [distance_traveled[0],distance_traveled[1]]
-            print 'first_duplicate' + str(first_duplicate)
-            duplicate_found = 1
+    # if duplicate_found == 0:
+    #     if distance_traveled in location_map:
+    #         print 'location_found'
+    #         print distance_traveled
+    #         first_duplicate = [distance_traveled[0],distance_traveled[1]]
+    #         print 'first_duplicate' + str(first_duplicate)
+    #         duplicate_found = 1
 
     location_map.append([distance_traveled[0],distance_traveled[1]])
-    print 'loc_map' + str(location_map)
+    #print 'loc_map' + str(location_map)
 
 
 print distance_traveled
@@ -70,6 +96,7 @@ print location_map
 
 print 'first_duplicate' + str(first_duplicate)
 duplicate_distance = abs(first_duplicate[0]) + abs(first_duplicate[1])
+print 'position_set' + str(positions)
 print 'duplicate_distance = ' + str(duplicate_distance)
 
 
